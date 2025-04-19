7969d48d796bd44bb9cd1c75038b8f33b6abc486
Justin halle <shpaw415@gmail.com>
2025-04-19 17:41:02 -0400

    bun create workaround

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,62 @@ import { input, confirm } from "@inquirer/prompts";
 import { mkdirSync } from "node:fs";
 import { CreateServerConfig } from "./serverConfig";
 
-const name = await input({
-  message: "Project Name?",
-});
+type Options = {
+  name: string;
+  tailwind: boolean;
+};
 
-const tailwind = await confirm({
-  message: "Do you want to use Tailwindcss?",
-});
-
-mkdirSync(name, { recursive: true });
-console.log(`Creating project ${name}...`);
-
-await Bun.$`bun init -y`.cwd(name);
-await Bun.$`bun i @bunpmjs/bunext`.cwd(name);
-await Bun.$`bun @bunpmjs/bunext/bin/index.ts init`.cwd(name);
-
-if (tailwind) {
-  await Bun.$`bun i tailwindcss @tailwindcss/cli`.cwd(name);
-  await Bun.file(`${name}/config/server.ts`).write(
-    CreateServerConfig({ tailwind })
-  );
+function execute(cmd: string, cwd: string) {
+  return Bun.$`bun ${cmd}`.cwd(cwd);
 }
+
+function createInstallList(options: Options) {
+  const { tailwind } = options;
+  const list = ["@bunpmjs/bunext", "@bunpmjs/bunext/bin/index.ts"];
+  if (tailwind) {
+    list.push("tailwindcss", "@tailwindcss/cli");
+  }
+  return list;
+}
+
+async function GetOptionsFromUser(): Promise<Options> {
+  const name = await input({
+    message: "Project Name?",
+  });
+  const tailwind = await confirm({
+    message: "Do you want to use Tailwindcss?",
+  });
+  return {
+    name,
+    tailwind,
+  };
+}
+
+async function InstallBunext(options: Options) {
+  const executeList = [
+    "init -y",
+    `install ${createInstallList(options).join(" ")}`,
+    "@bunpmjs/bunext/bin/index.ts init",
+  ];
+  const { name } = options;
+
+  for await (const cmd of executeList) {
+    await execute(cmd, name);
+  }
+}
+
+async function Main() {
+  const options = await GetOptionsFromUser();
+  mkdirSync(options.name, { recursive: true });
+  console.log(`Creating project ${options.name}...`);
+
+  await InstallBunext(options);
+
+  if (options.tailwind) {
+    await Bun.file(`${options.name}/config/server.ts`).write(
+      CreateServerConfig({ tailwind: options.tailwind })
+    );
+  }
+}
+
+await Main();
